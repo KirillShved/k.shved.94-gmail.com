ade2c4a9f90356f8c4e5c107bae77d8828ea87c5
kirill.shved <kirill.shved@cleverlabs.io>
2020-03-30 12:48:37 +0300

    Add 'Loader' component.

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -7,8 +7,9 @@
       @add-todo="addTodo"
     />
     <hr>
+    <Loader v-if="loading"></Loader>
     <TodoList
-      v-if="todos.length"
+      v-else-if="todos.length"
       v-bind:todos="todos"
       @remove-todo-item="removeTodoItem"
     />
@@ -18,23 +19,28 @@
 
 <script>
   import TodoList from '@/components/TodoList'
-import AddTodo from '@/components/AddTodo'
+  import AddTodo from '@/components/AddTodo'
+  import Loader from '@/components/Loader'
   export default {
     name: 'App',
     data() {
       return {
-        todos: []
+        todos: [],
+        loading: true,
       }
     },
     mounted() {
       fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
         .then(response => response.json())
         .then(json => {
-          this.todos = json
+          setTimeout(() => {
+              this.todos = json
+              this.loading = false
+            }, 1000)
         })
     },
     components: {
-      TodoList, AddTodo,
+      TodoList, AddTodo, Loader,
     },
     methods: {
       removeTodoItem(id) {
