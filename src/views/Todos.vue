612d6658f5e29f12213d9ebd3bae25073cebd856
kirill.shved <kirill.shved@cleverlabs.io>
2020-03-30 13:10:08 +0300

    Add computed filter.

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -6,11 +6,16 @@
     <AddTodo
       @add-todo="addTodo"
     />
+    <select v-model="filter">
+      <option value="all">All</option>
+      <option value="completed">Completed</option>
+      <option value="not-completed">Not Completed</option>
+    </select>
     <hr>
     <Loader v-if="loading"></Loader>
     <TodoList
-      v-else-if="todos.length"
-      v-bind:todos="todos"
+      v-else-if="filteredTodos.length"
+      v-bind:todos="filteredTodos"
       @remove-todo-item="removeTodoItem"
     />
     <p v-else>No todos!</p>
@@ -22,11 +27,12 @@
   import AddTodo from '@/components/AddTodo'
   import Loader from '@/components/Loader'
   export default {
-    name: 'App',
+    name: 'app',
     data() {
       return {
         todos: [],
         loading: true,
+        filter: 'all',
       }
     },
     mounted() {
@@ -38,6 +44,26 @@
               this.loading = false
             }, 1000)
         })
+    },
+    // watch: {
+    //   filter(value) {
+    //     console.log(value)
+    //   }
+    // },
+    computed: {
+      filteredTodos() {
+        if (this.filter === 'all') {
+          return this.todos
+        }
+
+        if (this.filter === 'completed') {
+          return this.todos.filter(t => t.completed)
+        }
+
+        if (this.filter === 'not-completed') {
+          return this.todos.filter(t => !t.completed)
+        }
+      }
     },
     components: {
       TodoList, AddTodo, Loader,
